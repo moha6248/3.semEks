fe0d0587f73ae28008e12d8177255ce3160214eb
moha6248 <51365122+moha6248@users.noreply.github.com>
2021-01-27 11:03:14 +0100

    test

--- programmering-eksam2/src/main/java/com/example/demo/DemoApplication.java
+++ programmering-eksam2/src/main/java/com/example/demo/DemoApplication.java
@@ -9,7 +9,7 @@ DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
-
+        System.out.println("eksamtestgi");
     }
 
 
